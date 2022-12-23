c51e00ffe701dcfc2c1a633543542b3dc0f821a3
Leandro Escalaera <marcelo.escalera.inf@gmail,com>
2022-12-23 00:45:20 -0400

    Auto stash before rebase of "origin/develop"
    +20221223: avance de ejercicio 13 :D

--- MiPrimerProyecto/src/miprimerproyecto/MiPrimerProyecto.java
+++ MiPrimerProyecto/src/miprimerproyecto/MiPrimerProyecto.java
@@ -2,35 +2,66 @@ package miprimerproyecto;
 
 /**
  *
- * @author <a href="mailto:leandro.escalera@bancofie.com.bo">Leandro Marcelo Escalera Alconini</a>
+ * @author <a href="mailto:leandro.escalera@bancofie.com.bo">Leandro Marcelo
+ * Escalera Alconini</a>
  */
 public class MiPrimerProyecto {
 
-  /**
-   * @param args the command line arguments
-   */
-  static public String invertir(String string) {
-    String strAux = "";
-    for (int i = string.length() - 1; i >= 0; i--) {
-      strAux += string.charAt(i);
+    /**
+     * @param args the command line arguments
+     */
+    static public String invertir(String string) {
+        String strAux = "";
+        for (int i = string.length() - 1; i >= 0; i--) {
+            strAux += string.charAt(i);
+        }
+        return strAux;
     }
-    return strAux;
-  }
-  
-  static public String solution(String inpuntString){
-    
-    
-    return null;
-  }
 
-  public static void main(String[] args) {
-    // TODO code application logic here
+    static public String quitarParentesis(String inputString) {
+        String auxString = inputString.replaceAll("[()]", "");
+        return auxString;
+    }
 
-    String str = "Hola Leandro como estas?";
+    static public String buscarParentesis(String inputString) {
+        String auxSubCadena = "";
+        String auxSubCadena2 = "";
+        Integer tamaño = inputString.length()-1;
+        for (int i = 0; i <= tamaño; i++) {
+            if (inputString.charAt(i) == '(') {
+                for (int j = i+1; j <= tamaño; j++) {
+                    if (inputString.charAt(j) == ')') {
+                        auxSubCadena = inputString.substring(i, j-1);
+                        auxSubCadena = quitarParentesis(auxSubCadena);
+                        auxSubCadena = invertir(auxSubCadena);
+                        
+                    }
 
-    System.out.println(invertir(str));
-    
+                }
+            }else{
+                auxSubCadena2+=inputString.charAt(i);
+            }
 
-  }
+        }
+
+        return auxSubCadena2;
+    }
+
+    static public String solution(String inputString) {
+
+        String stringSolution = buscarParentesis(inputString);
+        return stringSolution;
+    }
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+
+        String str = "(bar)";
+
+        String var = solution(str);
+        //System.out.println(invertir(str));
+        System.out.println(var);
+
+    }
 
 }
